647f9da4569d92e8b6edb9258728a0fd23c825eb
VirusMIX <teleport6500@gmail.com>
2023-12-26 19:43:45 +0300

    main

---
+++ main.py
@@ -0,0 +1,11 @@
+def bubble_sort(nums):
+    n = len(nums)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if nums[j] < nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+
+numbers = [5, 2, 9, 1, 6]
+bubble_sort(numbers)
+print(numbers)
